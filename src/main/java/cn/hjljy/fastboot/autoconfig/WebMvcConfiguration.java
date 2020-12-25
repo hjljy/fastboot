b517cbc79e347e6dfc6e25cc3bade5fce5a2c160
yichaofan <ycf921244819@hotmail.com>
2020-12-25 18:04:42 +0800

    完善request body内容无法多次读取的问题

---
+++ src/main/java/cn/hjljy/fastboot/autoconfig/WebMvcConfiguration.java
@@ -0,0 +1,21 @@
+package cn.hjljy.fastboot.autoconfig;
+
+import cn.hjljy.fastboot.autoconfig.filter.RequestBodyFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebMvcConfiguration {
+
+    @Bean
+    public FilterRegistrationBean setLogServiceFilter(){
+        FilterRegistrationBean<RequestBodyFilter> registrationBean = new FilterRegistrationBean<>();
+        RequestBodyFilter requestBodyFilter = new RequestBodyFilter();
+        registrationBean.setFilter(requestBodyFilter);
+        registrationBean.setName("过滤器处理请求body参数");
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
+}
