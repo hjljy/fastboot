069fee58dd60e674906502846bbb48eb884545bb
hjljy <hjljy@outlook.com>
2021-07-29 15:44:46 +0800

    新增获取每周，每月的第一天，最后一天

--- src/test/java/cn/hjljy/fastboot/FastMainTest.java
+++ src/test/java/cn/hjljy/fastboot/FastMainTest.java
@@ -1,12 +1,15 @@
 package cn.hjljy.fastboot;
 
 
+import cn.hjljy.fastboot.common.utils.LocalDateTimeUtil;
 import org.apache.commons.codec.Charsets;
 import org.springframework.security.authentication.BadCredentialsException;
 
 import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.*;
 
 /**
@@ -19,18 +22,8 @@ import java.util.*;
 public class FastMainTest {
 
     public static void main(String[] args) {
-       String header = "d2ViLWlvdDoxMjM0NTY3ODk";
-       byte[] base64Token = header.getBytes(StandardCharsets.UTF_8);
-
-        byte[] decoded;
-        try {
-            decoded = Base64.getDecoder().decode(base64Token);
-        } catch (IllegalArgumentException var7) {
-            throw new BadCredentialsException("Failed to decode basic authentication token");
-        }
-
-        String token = new String(decoded, StandardCharsets.UTF_8);
-        System.out.println(token);
+        List<LocalDate> dates = LocalDateTimeUtil.allDaysOfMonth(LocalDate.now());
+        System.out.println(dates);
     }
     public static Date str2Date(String date, String f) {
         SimpleDateFormat format = new SimpleDateFormat(f);
