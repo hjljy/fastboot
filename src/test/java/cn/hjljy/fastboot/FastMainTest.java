f2a53762ca078e292c35faac4aa13302024a571e
hjljy <hjljy@outlook.com>
2021-10-15 15:38:09 +0800

    处理JacksonUtil无法序列化LocalDateTime的问题

--- src/test/java/cn/hjljy/fastboot/FastMainTest.java
+++ src/test/java/cn/hjljy/fastboot/FastMainTest.java
@@ -14,6 +14,7 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author hjljy
